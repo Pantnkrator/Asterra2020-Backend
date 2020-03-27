33646f53743a769120eba3df8f08211ed584b6c7
Jorge <ras2145@gmail.com>
2020-03-27 15:49:45 -0400

    add interval to since id search

--- relevant_tweets.js
+++ relevant_tweets.js
@@ -28,40 +28,56 @@ var twitterSchema = new mongoose.Schema({
   fullTweet: String 
 });
 var Tweet = mongoose.model('Tweets', twitterSchema);
+let createTweet = new Tweet({});
+createTweet.save();
 
-
-setInterval(()=> {
-  console.log("CAll search Api ");
-  client.get('search/tweets', {q: 'flooding',geocode:'38.903219,-77.026713,100km',tweet_mode:'extended'}, function(error, tweets, response) {
-    console.log("total tweets: ",tweets.statuses.length);  
-    tweets.statuses.forEach(function(tweet) {
-      if(tweet.geo_enabled ) {
-       let newTweet = new  Tweet( {
-        created_at: toString( tweet.created_at ),
-        id: tweet.id,
-        id_str: JSON.stringify(tweet.id_str),
-        full_text: (tweet.full_text), 
-        entities: JSON.stringify(tweet.entities), 
-        user: JSON.stringify(tweet.user),
-        geo: JSON.stringify(tweet.geo),
-        coordinates: JSON.stringify(tweet.coordinates),
-        fullTweet: JSON.stringify(tweet)
-      });
-      Tweet.find({id:tweet.id}, function (err, docs) {
-          if (docs.length){
-              console.log("Already in DB", tweet.id);
-          }else{
-              console.log("Save Tweet", tweet.id);
-              newTweet.save(function(err){
-                  if(err) {
-                    console.log("Failed saving ", err);
-                  }
+  console.log("Call search Api ");
+  setInterval(()=>{
+    Tweet.findOne().sort({id_str:-1}).then((docs)=>{  
+      console.log("Max Id",docs.id_str);
+      if(!docs) { docs = {id_str:"12222220000000"}}
+      if(docs) {
+        let config = {q: 'flooding',geocode:'38.903219,-77.026713,100km',tweet_mode:'extended', count:100, since_id: docs.id_str};
+        console.log(config);
+        client.get('search/tweets', config , function(error, tweets, response) {
+          if(error) {
+            console.log("ERROR", error);
+          } else {
+            tweets.statuses.forEach(function(tweet) {
+              if(tweet.geo_enabled) {
+                // console.log(tweet);
+                let newTweet = new  Tweet( {
+                created_at: tweet.created_at.toString(),
+                id: tweet.id,
+                id_str: tweet.id_str,
+                full_text: (tweet.full_text), 
+                entities: JSON.stringify(tweet.entities), 
+                user: JSON.stringify(tweet.user),
+                geo: JSON.stringify(tweet.geo),
+                coordinates: JSON.stringify(tweet.coordinates),
+                fullTweet: JSON.stringify(tweet)
               });
+              Tweet.find({id:tweet.id}, function (err, docs) {
+                  if (docs.length){
+                      console.log("Already in DB", tweet.id);
+                  }else{
+                      console.log("Save Tweet", tweet.id);
+                      newTweet.save(function(err){
+                          if(err) {
+                            console.log("Failed saving ", err);
+                          }
+                      });
+                  }
+                });
+              }
+            });
           }
+          console.log("total tweets: ",tweets.statuses.length);  
+          
         });
       }
-     });
-  });
+    
+    });
+  }, 180000);
   
   
-},400000 );
