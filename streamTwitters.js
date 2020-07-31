e95a6b5574274c017a3e94c7333b5037120b8d38
Jorge <ras2145@gmail.com>
2020-07-31 11:01:32 -0400

    change twiitts

--- streamTwitters.js
+++ streamTwitters.js
@@ -59,8 +59,8 @@ console.log("STARTING TWEETS STREAM");
 stream.on('tweet', function (tweet) {
   console.log("Tweet enters", tweet.text);
   if(tweet.geo || tweet.coordinates) {
-    if ( tweet.text.includes(filters[0]) || tweet.text.includes(filters[1]) || tweet.text.includes(filters[2]) || tweet.text.includes(filters[3]) || tweet.text.includes(filters[4])  || tweet.text.includes(filters[5]) || tweets.text.includes(filters[6]) || tweets.text.includes(filters[7]) || tweets.text.includes(filters[8]) || tweets.text.includes(filters[9])
- || tweets.text.includes(filters[10]) || tweets.text.includes(filters[11])   ) {
+    if ( tweet.text.includes(filters[0]) || tweet.text.includes(filters[1]) || tweet.text.includes(filters[2]) || tweet.text.includes(filters[3]) || tweet.text.includes(filters[4])  || tweet.text.includes(filters[5]) || tweet.text.includes(filters[6]) || tweet.text.includes(filters[7]) || tweet.text.includes(filters[8]) || tweet.text.includes(filters[9])
+ || tweet.text.includes(filters[10]) || tweet.text.includes(filters[11])   ) {
       console.log(tweet.text);
       insertTwitter(tweet);
     } 
